30db2efa078a03ec04f6534a3938b21e6e596a90
pattarsuraj <pattarsuraj@gmail.com>
2021-02-22 19:18:31 +0900

    :zap: ROS2 Services

--- my_py_pkg/add_two_ints_client.py
+++ my_py_pkg/add_two_ints_client.py
@@ -10,6 +10,8 @@ class AddTwoIntsClientNode(Node):
     def __init__(self):
         super().__init__("add_two_ints_client")
         self.call_add_two_ints_server(6, 7)
+        # You can call them mutliple times for here
+        #self.call_add_two_ints_server(4, 3)
 
     def call_add_two_ints_server(self, a, b):
         client = self.create_client(AddTwoInts, "add_two_ints")
@@ -24,12 +26,12 @@ class AddTwoIntsClientNode(Node):
         # This add done callback is part of the future method and has nothing to do with
         # ROS. Here we use it since in python we can't wait asynchronously without
         # multi-threaded operation
-        future.add_done_callback(partial(self.callback_call_add_two_ints), a=a, b=b)
+        future.add_done_callback(partial(self.callback_call_add_two_ints, a=a, b=b))
 
     def callback_call_add_two_ints(self, future, a, b):
         try:
             response = future.result()
-            self.get_logger().info(str(request.a) + " + " + str(request.b) + " = " +
+            self.get_logger().info(str(a) + " + " + str(b) + " = " +
                     str(response.sum))
         except Exception as e:
             self.get_logger().error("Service call failed %r" % (e,))
--- my_py_pkg/number_counter.py
+++ my_py_pkg/number_counter.py
@@ -2,7 +2,7 @@
 import rclpy
 from rclpy.node import Node
 from example_interfaces.msg import Int64
-from example_interfaces.msg import String
+from example_interfaces.srv import SetBool
  
  
 class NumberCounterNode(Node):
@@ -15,19 +15,39 @@ class NumberCounterNode(Node):
                                                     self.callback_number_counter,
                                                     10)
         self.publisher_ = self.create_publisher(Int64, "number_count", 10)
-        #self.timer_ = self.create_timer(0.5, self.callback_number_counter)
+
+        # Reset counter service
+        self.server_ = self.create_service(SetBool, "reset_counter",
+                                           self.callback_reset_counter)
                                                 
         self.get_logger().info("Number counter has started.")
 
     def callback_number_counter(self, msg):
+        # Subscriber
         self.counter_ += msg.data
         self.get_logger().info("Number count is: " + str(self.counter_))
 
+        # Publisher
         new_msg = Int64()
         new_msg.data = self.counter_
         self.publisher_.publish(new_msg)
 
- 
+    def callback_reset_counter(self, request, response):
+        # Service
+        if request.data:
+            self.counter_ = 0
+            response.success = True
+            response.message = "Counter has been reset"
+        else:
+            response.success = False
+            response.message = "Counter has not been reset"
+
+        return response
+
+
+        bool.success
+
+
 def main(args=None):
     rclpy.init(args=args)
     node = NumberCounterNode()
